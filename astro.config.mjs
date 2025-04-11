eb6a246c3889f0446cd5ed124d0590ca84d3e686
Alex <basurillodesgastorito@gmail.com>
2025-04-11 19:57:21 +0200

    Header mobile improvements.

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,7 +12,7 @@ export default defineConfig({
     integrations: [icon({
         iconDir: "public/icons",
         include: {
-            "lucide": ["info", "film", "house", "mail-open", "newspaper", "book", "arrow-right", "circle-alert", "server", "package", "package-open", "users", "tag", "id-card", "stamp", "bookmark", "disc", "shield", "feather", "moon", "sun", "arrow-up-right", "menu", "earth", "shopping-basket", "smile", "zap", "chart-pie", "video", "mic", "tv", "circle", "truck", "camera", "bot", "leaf", "chevron-down", "presentation", "umbrella", "mail", "rss", "chevron-left", "chevron-right", "brush", "refresh-cw", "shopping-bag", "shopping-cart"],
+            "lucide": ["info", "film", "house", "mail-open", "newspaper", "book", "arrow-right", "circle-alert", "server", "package", "package-open", "users", "tag", "id-card", "stamp", "bookmark", "disc", "shield", "feather", "moon", "sun", "arrow-up-right", "menu", "earth", "shopping-basket", "smile", "zap", "chart-pie", "video", "mic", "tv", "circle", "truck", "camera", "bot", "leaf", "chevron-down", "presentation", "umbrella", "mail", "rss", "chevron-left", "chevron-right", "brush", "refresh-cw", "shopping-bag", "shopping-cart", "home", "box"],
             "simple-icons": ["twitter", "bluesky", "instagram", "youtube"]
         }
     })],
